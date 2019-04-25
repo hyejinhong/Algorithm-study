36c20e896c58a7cbdd83875552eacc3c1d7b6735
Hyejin Hong <hhj732@naver.com>
2019-04-25 19:53:47 +0900

    190425

---
+++ Problem Solving/Codeforces 455A Boredom.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+long long max(long long n, long long m) {
+	if(n>=m)
+		return n;
+	else
+		return m;
+} 
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	long long a;
+	long long c[100010]; // 숫자 몇개씩 있는지 count해서 저장 
+	long long dp[100010]; // dp[i] : i~N 범위 중 가장 최대값, 따라서 dp[1]이 답이 됨 
+	
+	for(int i=0; i<n; i++) { 
+		cin >> a;
+		c[a]++;
+	}
+	 
+	dp[0] = 0;
+	dp[1] = c[1];
+	
+	for(int i=2; i<100001; i++) {
+		dp[i] = max(dp[i-1], dp[i-2]+c[i]*i);
+	}
+	
+	cout << dp[100000] << endl;
+}
