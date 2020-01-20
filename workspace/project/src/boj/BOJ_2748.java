09cf64bd603457284e6620a1ecc519aa96015479
Hyejin Hong <hhj732@naver.com>
2020-01-20 14:44:43 +0900

    200120

---
+++ workspace/project/src/boj/BOJ_2748.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.util.*;
+
+public class BOJ_2748 {
+	
+	static long[] arr = new long[91];
+	static int n;
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		
+		Arrays.fill(arr, -1);
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		set(2);
+		
+		System.out.println(arr[n]);
+	}
+	
+	public static void set(int m) {
+		// ±âÀú
+		if(m > n) {
+			return;
+		}
+		
+		arr[m] = arr[m-1] + arr[m-2];
+		set(m+1);
+	}
+}
