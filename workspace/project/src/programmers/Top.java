951c87bc7ea8625bc764f347a14a5310fcee1efb
Hyejin Hong <hhj732@naver.com>
2020-04-02 12:51:33 +0900

    200402
    프로그래머스 탑

---
+++ workspace/project/src/programmers/Top.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Top {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] heights = {1,5,3,6,7,6,5};
+		int[] answer = new int[heights.length];
+		
+//		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=heights.length-1; i>0; i--) {
+			// 왼쪽 탑에 수신 가능
+			if(heights[i-1] > heights[i]) {
+				answer[i] = (i-1) + 1;
+			}
+			// 수신 못하면 왼쪽으로 가면서 높은 탑 찾음
+			else {
+				for(int j=i-1; j>=0; j--) {
+					// 왼쪽 탑에 수신 가능
+					if(heights[j] > heights[i]) {
+						answer[i] = j+1;
+						break;
+					}
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
