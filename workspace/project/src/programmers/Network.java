5b0844e80ca825346b5fd1c0b2887a679968844c
Hyejin Hong <hhj732@naver.com>
2020-04-04 14:36:53 +0900

    200404
    프로그래머스 네트워크

---
+++ workspace/project/src/programmers/Network.java
@@ -0,0 +1,60 @@
+package programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Network {
+
+	static boolean[] visited = new boolean[200];
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+//		int[][] computers = {
+//				{1, 1, 0},
+//				{1, 1, 0},
+//				{0, 0, 1}
+//		};
+		
+		int[][] computers = {
+				{1, 1, 0},
+				{1, 1, 1},
+				{0, 1, 1}
+		};
+
+		System.out.println(solution(n, computers));
+	}
+
+	public static int solution(int n, int[][] computers) {
+		int answer = 0;
+		
+		for(int i=0; i<n; i++) {
+			// 방문하지 않은 정점에서부터 bfs 시작
+			if(!visited[i]) {
+				bfs(i, n, computers);
+				answer++;
+			}
+		}
+		return answer;
+	}
+	
+	public static void bfs(int start, int n, int[][] computers) {
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(start);
+		visited[start] = true;
+		
+		while(!q.isEmpty()) {
+			int here = q.poll();
+			visited[here] = true;
+			
+			// 모든 정점 검사
+			for(int i=0; i<n; i++) {
+				// 인접하고 아직 방문하지 않은 곳이면 큐에 추가함
+				if(computers[here][i] == 1 && !visited[i]) {
+					q.offer(i);
+				}
+			}
+		}
+	}
+}
