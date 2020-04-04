a557aecdd68f92a492da1ac2e6d31ade6fdbd614
Hyejin Hong <hhj732@naver.com>
2020-04-04 17:19:36 +0900

    200404
    [15649] N과 M (1) - 백 트래킹

---
+++ workspace/project/src/boj/BOJ_15649.java
@@ -0,0 +1,52 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_15649 {
+
+	static int n, m;
+	static boolean[] check = new boolean[9];
+	static ArrayList<Integer> picked = new ArrayList<Integer>();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(stk.nextToken());
+		m = Integer.parseInt(stk.nextToken());
+		
+		pick();
+	}
+
+	public static void pick() {
+		// ±‚¿˙ : ¥Ÿ ªÃ¿Ω
+		if(picked.size() == m) {
+			print();
+			return;
+		}
+		
+		for(int i=1; i<=n; i++) {
+			if(!check[i]) {
+				picked.add(i);
+				check[i] = true;
+				
+				pick();
+				
+				picked.remove(picked.size()-1);
+				check[i] = false;
+			}
+		}
+	}
+	
+	public static void print() {
+		for(int num : picked) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+}
