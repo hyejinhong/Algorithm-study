5e5f8173a17c0ae00bb2988d2ed37b68c8085c71
Hyejin Hong <hhj732@naver.com>
2020-04-13 18:33:02 +0900

    200413
    [11725] 트리의 부모 찾기 - 트리 (BFS 추가)

--- workspace/project/src/boj/BOJ_11725.java
+++ workspace/project/src/boj/BOJ_11725.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_11725 {
@@ -35,7 +37,8 @@ public class BOJ_11725 {
 		}
 		
 		parents = new int[n+1];
-		dfs(1, 0);
+//		dfs(1, 0);
+		bfs(1, 0);
 		for(int i=2; i<=n; i++) {
 			System.out.println(parents[i]);
 		}
@@ -49,6 +52,28 @@ public class BOJ_11725 {
 			int next = adj[start].get(i);
 			if(!visited[next]) {
 				dfs(next, start);
+			}
+		}
+	}
+	
+	public static void bfs(int start, int parent) {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] discovered = new boolean[n+1];
+		discovered[start] = true;
+		
+		q.add(start);
+		
+		while(!q.isEmpty()) {
+			int here = q.poll();
+			
+			for(int i=0; i<adj[here].size(); i++) {
+				int next = adj[here].get(i);
+				
+				if(!discovered[next]) {
+					discovered[next] = true;
+					q.add(next);
+					parents[next] = here;
+				}
 			}
 		}
 	}
