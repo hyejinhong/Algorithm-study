5195ffeaf0bb55e9d1e65735cc0bb2393e54439b
Hyejin Hong <hhj732@naver.com>
2020-05-19 15:14:53 +0900

    200519
    프로그래머스 큰 수 만들기

---
+++ workspace/project/src/programmers/MakingLargeNumber.java
@@ -0,0 +1,41 @@
+package programmers;
+
+public class MakingLargeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] number = { "1924", "1231234", "4177252841" };
+		int[] k = { 2, 3, 4 };
+		for(int i=0; i<number.length; i++) {
+			System.out.println(solution(number[i], k[i]));
+		}
+	}
+
+	public static String solution(String number, int k) {
+		StringBuilder sb = new StringBuilder(number);
+		
+		// 내 뒤에 있는 숫자가 나보다 크면 없앤다.
+		int count = 0;
+		int index = 1;
+        
+		while(count < k) {
+			if(index >= 1 && sb.charAt(index) > sb.charAt(index-1)) {
+				sb.deleteCharAt(index-1);
+				index--;
+				count++;
+			}
+			else {
+				if(index == sb.length()-1 && sb.charAt(index) <= sb.charAt(index-1)) {
+					sb.deleteCharAt(index);
+					count++;
+					index--;
+				}
+				else {
+					index++;
+				}
+			}
+		}
+				
+		return sb.toString();
+	}
+}
