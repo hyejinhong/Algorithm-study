6a59b7a5d28bd1e94ab966534053f3ff02654bee
Hyejin Hong <hhj732@naver.com>
2020-05-26 15:55:35 +0900

    200526
    프로그래머스 같은 숫자는 싫어

---
+++ workspace/project/src/programmers/IHateSameNumbers.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IHateSameNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = {
+				{1,1,3,3,0,1,1}, {4,4,4,3,3}
+		};
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(Arrays.toString(solution(arr[i])));
+		}
+	}
+
+	public static int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for(int i=1; i<arr.length; i++) {
+            int number = arr[i];
+            
+            if(arr[i-1] != number) {
+                list.add(number);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+
+        return answer;
+	}
+}
