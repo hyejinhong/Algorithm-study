16a873cbd8cdcf61b7a4c361fdd9911a6eba34f7
hyejinhong <hhj732@naver.com>
2022-03-30 20:13:46 +0900

    Update BOJ_1717.java

--- workspace/project/src/boj/BOJ_1717.java
+++ workspace/project/src/boj/BOJ_1717.java
@@ -3,9 +3,6 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class BOJ_1717 {
