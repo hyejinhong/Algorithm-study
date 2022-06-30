72fb7902b3df15cc8624be6e6b9c0924fe3de47b
hyejinhong <hhj732@naver.com>
2022-06-30 21:45:31 +0900

    220630
    LeetCode 2078 Two Furthest housese with diffrent colors

---
+++ workspace/project/src/leetcode/LeetCode_2078.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+public class LeetCode_2078 {
+    public static void main(String[] args) {
+
+//        int[] colors = { 1, 1, 1, 6, 1, 1, 1 };
+//        int[] colors = { 1, 8, 3, 8, 3 };
+        int[] colors = { 0, 1 };
+
+        System.out.println(solution(colors));
+    }
+
+    public static int solution(int[] colors) {
+        int max = 0;
+
+        for(int std=0; std<colors.length; std++) {
+            int target = std+1;
+            while(target < colors.length) {
+                // 색 같으면
+                if(colors[std] == colors[target]) {
+                    target++;
+                }
+
+                // 색 다르면 거리 갱신
+                else {
+                    max = Math.max(max, Math.abs(target-std));
+                    target++;
+                }
+            }
+        }
+
+        return max;
+    }
+
+//    public static int maxDistance(int[] colors) {
+//        int n = colors.length;
+//        int start = 0;
+//        int end = 0;
+//        int maxDistance = 0;
+//
+//        while(start < n) {
+//            // start == end -> end 한칸 뒤로
+//            if(colors[start] == colors[end])
+//                end++;
+//
+//            // start != end ->
+//            else if(colors[start] != colors[end]) {
+//
+//            }
+//        }
+//    }
+}
